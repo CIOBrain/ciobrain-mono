e36167839c3b7f709996fe10756ad3182cc4719e
johnW_ret <19534013+johnW-ret@users.noreply.github.com>
2023-03-24 14:15:02 -0500

    Added preliminal swagger API documentation

--- src/ciobrain-api/index.js
+++ src/ciobrain-api/index.js
@@ -3,9 +3,30 @@ import cors from "cors"
 import assetRouter from "./src/routes/asset.js"
 import authenticateRouter from "./src/routes/authenticate.js";
 import loggingController from "./src/controller/loggingController.js"
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 
 const PORT = process.env.PORT || 3002
 const app = express()
+
+// setup swagger-jsdoc swagger gen
+//const swaggerJsdoc = require('swagger-jsdoc');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Hello World',
+      version: '1.0.0',
+    },
+  },
+  apis: ['./src/routes/*.js'], // files containing annotations as above
+}
+
+const openapiSpecification = swaggerJSDoc(options);
+
+// setup swagger gen and host
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
 
 app.use(cors())
 app.options("*", cors())
